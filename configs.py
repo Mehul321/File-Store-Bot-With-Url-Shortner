8d838db9f782d811b3fab3674afdd899d388c9e1
Mehul321 <119873152+Mehul321@users.noreply.github.com>
2026-04-16 22:48:26 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,25 +5,36 @@
 import os
 
 class Config(object):
+  # --- TERI DETAILS (Hardcoded for GitHub) ---
   API_ID = int(os.environ.get("API_ID", "26386777"))
   API_HASH = os.environ.get("API_HASH", "ee7bbb1078fa4aaf4c1b6e9cfeec3ca1")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "7308784807:AAGa9IolcKZpclbOFjnAeC5G5AT4_3O7Z-0")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "Kingshortx_bot")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "8398492174:AAH9s6x3YYKVTMTKsceE-LAzSwJPr-B1CRg")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "BlackDevilking_bot")
   DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1003486068610"))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "kingshortx.in")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "3be6a14aec4f07c487ed881604fedc5fa2e7732c")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "910090161"))
   DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://mehulrathod8514:IpEFuQmV5zFUWd0B@cluster0.91zmh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1003379165829")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003432402350"))
-  BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
+  
+  # Shortener Settings
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "kingshortx.in")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "C62b3eb6a2fd5fa64d03dc8e2d06d616f2fc2be1") 
+  
+  # --- NEW: Automatic Batching Settings ---
+  # Bot 30 seconds wait karega saari files collect karne ke liye
+  BATCH_TIME = int(os.environ.get("BATCH_TIME", 30)) 
+  # ----------------------------------------
+
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003486068610"))
+  
+  BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split() if x.strip())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
-  BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
+  BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split() if x.strip()))
   OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
+  
   ABOUT_BOT_TEXT = f"""
-This is a Permanent FileStore Bot. 
-Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
+This is a Permanent FileStore Bot with Auto-Batching.
+Send me multiple files, wait for {BATCH_TIME} seconds, and get a single link!
 
 ╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
 │
@@ -38,16 +49,12 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
   ABOUT_DEV_TEXT = f"""
 🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
  
- I am Super noob Please Support My Hard Work.
-
-[Donate Me](https://t.me/KingVj01)
+I am Super noob Please Support My Hard Work.
 """
   HOME_TEXT = """
 Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
 
-How to Use Bot & it's Benefits??
-
-📢 Send me any File & It will be uploaded in My Database & You will Get the File Link.
-
-⚠️ Benefits: If you have a TeleGram Movie Channel or Any Copyright Channel, Then Its Useful for Daily Usage, You can Send Me Your File & I will Send Permanent Link to You & Channel will be Safe from **CopyRight Infringement** Issue. I support Channel Also You Can Check **About Bot**.
+**Auto-Batching is ON!**
+📢 Just send me all your files. I will wait for 30 seconds and give you a single link. No need to use /batch command!
 """
+  
